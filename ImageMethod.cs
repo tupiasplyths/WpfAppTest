9afa067dd0f8bb148c56cd43729f5e7a8322b14a
Tupi Asplyths <phereplyths@yandex.com>
2024-11-17 17:40:59 +0700

    add image utils and wpf-method

---
+++ ImageMethod.cs
@@ -0,0 +1,200 @@
+using System;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+// using Windows.Storage.Streams;
+using BitmapEncoder = System.Windows.Media.Imaging.BitmapEncoder;
+using BitmapFrame = System.Windows.Media.Imaging.BitmapFrame;
+using Point = System.Windows.Point;
+
+namespace WpfAppTest;
+
+public static class ImageMethods
+{
+    public static Bitmap PadImage(Bitmap image, int minW = 64, int minH = 64)
+    {
+        if (image.Height >= minH && image.Width >= minW)
+            return image;
+
+        int width = Math.Max(image.Width + 16, minW + 16);
+        int height = Math.Max(image.Height + 16, minH + 16);
+
+        // Create a compatible bitmap
+        Bitmap destination = new(width, height, image.PixelFormat);
+        using Graphics gd = Graphics.FromImage(destination);
+
+        gd.Clear(image.GetPixel(0, 0));
+        gd.DrawImageUnscaled(image, 8, 8);
+
+        return destination;
+    }
+
+    public static Bitmap GetRegionOfScreenAsBitmap(Rectangle region)
+    {
+        Bitmap bmp = new(region.Width, region.Height, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        using Graphics g = Graphics.FromImage(bmp);
+
+        g.CopyFromScreen(region.Left, region.Top, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+        bmp = PadImage(bmp);
+
+        // Singleton<HistoryService>.Instance.CacheLastBitmap(bmp);
+        return bmp;
+    }
+
+    public static BitmapImage BitmapToImageSource(Bitmap bitmap)
+    {
+        using MemoryStream memory = new();
+        // using WrappingStream wrapper = new(memory);
+
+        bitmap.Save(memory, ImageFormat.Bmp);
+        memory.Position = 0;
+        BitmapImage bitmapImage = new();
+        bitmapImage.BeginInit();
+        bitmapImage.StreamSource = memory;
+        bitmapImage.CacheOption = BitmapCacheOption.OnLoad;
+        bitmapImage.EndInit();
+        bitmapImage.StreamSource = null;
+        bitmapImage.Freeze();
+
+        memory.Flush();
+
+        return bitmapImage;
+    }
+
+    public static Bitmap GetWindowsBoundsBitmap(Window passedWindow)
+    {
+        DpiScale dpi = VisualTreeHelper.GetDpi(passedWindow);
+        int windowWidth = (int)(passedWindow.ActualWidth * dpi.DpiScaleX);
+        int windowHeight = (int)(passedWindow.ActualHeight * dpi.DpiScaleY);
+
+        Point absPosPoint = passedWindow.GetAbsolutePosition();
+
+        int thisCorrectedLeft = (int)(absPosPoint.X);
+        int thisCorrectedTop = (int)(absPosPoint.Y);
+
+        Bitmap bmp = new(windowWidth, windowHeight, System.Drawing.Imaging.PixelFormat.Format32bppArgb);
+        using Graphics g = Graphics.FromImage(bmp);
+
+        g.CopyFromScreen(thisCorrectedLeft, thisCorrectedTop, 0, 0, bmp.Size, CopyPixelOperation.SourceCopy);
+        return bmp;
+    }
+
+    public static ImageSource GetWindowBoundsImage(Window passedWindow)
+    {
+        Bitmap bmp = GetWindowsBoundsBitmap(passedWindow);
+        return BitmapToImageSource(bmp);
+    }
+
+    // public static Bitmap ScaleBitmapUniform(Bitmap passedBitmap, double scale)
+    // {
+    //     using MemoryStream memory = new();
+    //     using WrappingStream wrapper = new(memory);
+
+    //     passedBitmap.Save(wrapper, ImageFormat.Bmp);
+    //     wrapper.Position = 0;
+    //     BitmapImage bitmapImage = new();
+    //     bitmapImage.BeginInit();
+    //     bitmapImage.StreamSource = wrapper;
+    //     bitmapImage.CacheOption = BitmapCacheOption.None;
+    //     bitmapImage.EndInit();
+    //     bitmapImage.Freeze();
+
+    //     wrapper.Flush();
+
+    //     TransformedBitmap transformedBitmap = new();
+    //     transformedBitmap.BeginInit();
+    //     transformedBitmap.Source = bitmapImage;
+    //     transformedBitmap.Transform = new ScaleTransform(scale, scale);
+    //     transformedBitmap.EndInit();
+    //     transformedBitmap.Freeze();
+    //     return BitmapSourceToBitmap(transformedBitmap);
+
+    // }
+
+    // public static Bitmap InteropBitmapToBitmap(System.Windows.Interop.InteropBitmap source)
+    // {
+    //     Bitmap bmp = new(
+    //       source.PixelWidth,
+    //       source.PixelHeight,
+    //       System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+    //     BitmapData data = bmp.LockBits(
+    //       new Rectangle(System.Drawing.Point.Empty, bmp.Size),
+    //       ImageLockMode.WriteOnly,
+    //       System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+    //     source.CopyPixels(
+    //       Int32Rect.Empty,
+    //       data.Scan0,
+    //       data.Height * data.Stride,
+    //       data.Stride);
+    //     bmp.UnlockBits(data);
+    //     return bmp;
+    // }
+
+    // public static Bitmap BitmapSourceToBitmap(BitmapSource source)
+    // {
+    //     Bitmap bmp = new(
+    //       source.PixelWidth,
+    //       source.PixelHeight,
+    //       System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+    //     BitmapData data = bmp.LockBits(
+    //       new Rectangle(System.Drawing.Point.Empty, bmp.Size),
+    //       ImageLockMode.WriteOnly,
+    //       System.Drawing.Imaging.PixelFormat.Format32bppPArgb);
+    //     source.CopyPixels(
+    //       Int32Rect.Empty,
+    //       data.Scan0,
+    //       data.Height * data.Stride,
+    //       data.Stride);
+    //     bmp.UnlockBits(data);
+    //     return bmp;
+    // }
+
+    // public static Bitmap GetBitmapFromIRandomAccessStream(IRandomAccessStream stream)
+    // {
+    //     Bitmap bitmap = new(stream.AsStream());
+    //     return bitmap;
+    // }
+
+    // public static BitmapImage GetBitmapImageFromIRandomAccessStream(IRandomAccessStream stream)
+    // {
+    //     BitmapImage bmp = new();
+    //     Stream ioStream = stream.AsStream();
+    //     // Create a new BitmapImage and use the SetSourceAsync method to 
+    //     // initialize it from the given IRandomAccessStream.
+    //     bmp.BeginInit();
+    //     bmp.CacheOption = BitmapCacheOption.None;
+    //     bmp.StreamSource = ioStream;
+    //     bmp.EndInit();
+    //     bmp.Freeze();
+    //     return bmp;
+    // }
+
+    // internal static RotateFlipType GetRotateFlipType(string path)
+    // {
+    //     using Image img = Image.FromFile(path);
+    //     RotateFlipType rotateFlipType = img.GetRotateFlipType();
+    //     return rotateFlipType;
+    // }
+
+    internal static void RotateImage(BitmapImage droppedImage, RotateFlipType rotateFlipType)
+    {
+        // Only consider basic rotation for now
+        switch ((int)rotateFlipType)
+        {
+            case 1:
+                droppedImage.Rotation = Rotation.Rotate90;
+                break;
+            case 2:
+                droppedImage.Rotation = Rotation.Rotate180;
+                break;
+            case 3:
+                droppedImage.Rotation = Rotation.Rotate270;
+                break;
+            default:
+                break;
+        }
+    }
+}
--- MainWindow.xaml.cs
+++ MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Windows.Controls;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
+using System.Windows.Input;
 
 namespace WpfAppTest
 {
@@ -19,35 +20,69 @@ namespace WpfAppTest
             //Content = grid;
         }
 
+        public void SetImageToBackground()
+        {
+            BG.Source = null;
+            BG.Source = ImageMethods.GetWindowBoundsImage(this);
+            BG.Opacity = 0.2;
+        }
+
         private void Button_Click(object sender, RoutedEventArgs e)
         {
-            double left = Canvas.GetLeft(clip);
-            double top = Canvas.GetTop(clip);
-            double width = clip.Width;
-            double height = clip.Height;
 
-            RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
+            //RenderTargetBitmap renderTargetBitmap = new RenderTargetBitmap((int)width, (int)height, 96, 96, PixelFormats.Pbgra32);
 
             DrawingVisual drawingVisual = new DrawingVisual();
             using (DrawingContext drawingContext = drawingVisual.RenderOpen())
             {
                 VisualBrush visualBrush = new VisualBrush(vancas);
-                drawingContext.DrawRectangle(visualBrush, null, new Rect(new Point(-left, top), new Point(width - left, height - top)));
+                // drawingContext.DrawRectangle(visualBrush, null, new Rect(new Point(-left, top), new Point(width - left, height - top)));
             }
-            renderTargetBitmap.Render(drawingVisual);
+            //renderTargetBitmap.Render(drawingVisual);
 
             // Save the image to a file
             BitmapEncoder encoder = new PngBitmapEncoder();
-            encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
-            using (var stream = System.IO.File.Create("captured_image.png"))
+            //encoder.Frames.Add(BitmapFrame.Create(renderTargetBitmap));
+            using (var stream = System.IO.File.Create("./output/captured_image.png"))
             {
                 encoder.Save(stream);
             }
         }
 
-        private async void FreezeScreen() 
+        // private async void FreezeScreen() 
+        // {
+        //     BackgroundBrush.Opacity = 0;
+        // } 
+
+        private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e) {
+            TopButtonStack.Visibility = Visibility.Collapsed;
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {   
+            WindowState = WindowState.Maximized;
+            FullWindow.Rect = new System.Windows.Rect(0, 0, Width, Height);
+
+            if (IsMouseOver)
+            {
+                TopButtonStack.Visibility = Visibility.Visible;
+            }
+        }
+
+        private void Window_Unloaded(object sender, RoutedEventArgs e)
         {
-            BackgroundBrush.Opacity = 0;
-        } 
+            TopButtonStack.Visibility = Visibility.Collapsed;
+        }
+
+        private void RegionClickCanvas_MouseLeave(object sender, MouseEventArgs e)
+        {
+            TopButtonStack.Visibility = Visibility.Collapsed;
+        }
+
+        private void RegionClickCanvas_MouseEnter(object sender, MouseEventArgs e)
+        {
+            TopButtonStack.Visibility = Visibility.Visible;
+        }
+
     }
 }
